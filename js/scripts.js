4e560d222476cc9b157c355377aea0ca428485b7
Erica <garbererica@gmail.com>
2016-11-18 10:36:45 -0800

    add form styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-  $("#tickets").click(function(){
-    $(".tickets").delay(500).fadeIn(2000);
+  $("#apply").click(function(){
+    $(".apply").delay(500).fadeIn(2000);
     $(".project").fadeOut();
     $(".header li").removeClass("active");
     $(".header li:last").addClass("active");
   });
   $("#project").click(function(){
-    $(".tickets").fadeOut();
+    $(".apply").fadeOut();
     $(".project").delay(500).fadeIn(2000);
     $(".header li").removeClass("active");
     $(".header li:first").addClass("active");
